f060440057103fe5623d70598b802d03cd2754f3
Kevin Du <kdu2030@gmail.com>
2022-05-25 08:51:24 -0400

    Updated Top Bar

--- planner/src/Components/TopBar.tsx
+++ planner/src/Components/TopBar.tsx
@@ -1,8 +1,19 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar'
 
-function TopBar(){
-    return(
+/*
+<div className="top-bar">
+            <h1>Planner.com</h1>
+        </div>*/
+
+function TopBar() {
+    return (
         <div className="top-bar">
-            <h1>Planner App</h1>
+            <AppBar position="fixed" sx={{height: "50px"}}>
+                <Toolbar>
+                    <h1>Planner.com</h1>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 
